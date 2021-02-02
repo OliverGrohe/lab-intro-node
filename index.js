f7b05605cb16394064bbc6905f723e8c75554412
Oliver Grohe <o.grohe@gmail.com>
2021-02-02 18:15:33 +0100

    Solved Lab

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => (b > a ? -1 : 1))
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] == undefined) {
+      throw new Error ('OutofBounds')
+    }
+    else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length == 0) {
+      throw new Error ('EmptySortedList')
+    }
+    else {
+      this.items.sort((a,b) => (b < a ? -1 : 1))
+      return this.items[0]
+      }
+    }
 
-  min() {}
+  min() {
+    if(this.items.length == 0) {
+      throw new Error ('EmptySortedList')
+    }
+    else {
+      this.items.sort((a,b) => (b > a ? -1 : 1))
+      return this.items[0]
+      }
+    }
 
-  sum() {}
+  sum() {
+    
+  }
 
   avg() {}
 }
